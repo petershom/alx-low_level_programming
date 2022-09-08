4dc7d222c2fa5eaa48f2b25faf364ac950b52e31
petershom <petershom644@gmail.com>
2022-09-08 15:32:25 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+*
+*main- print the string using the put function
+*
+*this program prints "Programming is like building a multilingual puzzle
+*return: 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
