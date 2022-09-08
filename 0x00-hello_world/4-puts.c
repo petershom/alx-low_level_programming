35b15f2024573ec649efad23deda8153378cb6ff
petershom <petershom644@gmail.com>
2022-09-08 15:36:59 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
+#!/bin/bash
 /**
-*
 *main- print the string using the put function
 *
 *this program prints "Programming is like building a multilingual puzzle
@@ -10,3 +10,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
