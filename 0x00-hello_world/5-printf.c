6b2ab9089dfa87d959f183d26371bdda2c2185b1
petershom <petershom644@gmail.com>
2022-09-08 17:31:14 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * Description: Use the function printf
  * this program prints 'with proper grammar, but the outcome is a piece of art,
  * Return: 0 
-*/
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
