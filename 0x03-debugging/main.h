4ac41320814e0dd2fc26a193dcae4fc155b37aea
petershom <petershom644@gmail.com>
2022-09-16 04:47:13 +0100

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
-#[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
 #define MAIN_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)[C[C[C[D[D[D[D[[C, int, int);
-void print_remaining_days(int, int, int);
-int convert_day[5~(int, int);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
+#endif /* MAIN_H */
