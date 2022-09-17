22f0e2227ef1bb78a7fc1608345acaecf8a73cad
petershom <petershom644@gmail.com>
2022-09-17 06:44:06 +0100

    more functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * main - header file for prototype
+ * void  prototype (void)
+ * int  prototype (int)
+ */
+
 int _putchar(char);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -12,6 +18,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
+oid  print_most_numbers(void); 
 
-#endif
+#endif /* MAIN_H */
