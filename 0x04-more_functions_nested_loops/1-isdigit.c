ac777f3fb3c501523623b611854ec48d90596c11
petershom <101981381+petershom@users.noreply.github.com>
2022-09-19 03:05:43 +0100

    Update 1-isdigit.c
     function that multiplies two integers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main - a function that checks for a digit 0 through 9
- *
- * Return: 1, 0
+ * _isdigit -  a function that checks for a digit 0-9
+ * @c: input
+ * Return: 1 if c is a digit, 0 otherwise
  */
-
 int _isdigit(int c)
 {
 	if ((c >= 48) && (c <= 57))
