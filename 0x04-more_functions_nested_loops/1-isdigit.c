ff4b4e03d1536d7d945913efef56c74285c591b8
petershom <petershom644@gmail.com>
2022-09-19 01:59:26 +0100

    function that multiplies two integers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,17 +5,10 @@
  *
  * Return: 1, 0
  */
-
-int _isdigit(int i);
-
-main(void)
+int _isdigit(int c)
 {
-	if ((i >= 0) && (i <= 9))
-	{
+	if ((c >= 48) && (c <= 57))
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
