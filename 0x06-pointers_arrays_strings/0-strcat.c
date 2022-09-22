af68a0533be2d291d6491eee8509fc55baa45930
petershom <petershom644@gmail.com>
2022-09-22 23:57:42 +0100

    more pointerffff

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,25 +4,19 @@
  * _strcat -  concatenates two strings
  * @dest: char
  * @src: char
- * Return: concatenate char
+ * Return: concatenate cr
  */
-
 char *_strcat(char *dest, char *src)
 {
-	char *p = dest;
 
-	while (*dest != '\0')
+	int i, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		dest++;
 	}
 
-	while (*src != '\0')
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
 	}
-	*dest = '\0';
-
-	return (p);
+	return (dest);
 }
