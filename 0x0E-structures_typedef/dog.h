9cb4edaed93a85973f3317e719764e83e1e8600c
petershom <petershom644@gmail.com>
2022-10-10 12:06:49 +0100

    Fixes a bad condition

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,28 +1,35 @@
-#include "main.h"
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdio.h>
-#include "dog.h"
+#include <stdlib.h>
 
-/*
- * struct dog - Define a new type struct dog
- * name@ : char
- * age@ : float
- * owner@ : char
- * Return: 0
- */
-
+/**
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
-}
-int main()
-{
-        struct dog my_dog;
-        my_dog.name = "Poppy";
-        my_dog.age = 3.5;
-        my_dog.owner = "Bob";
-        printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+char *name;
+float age;
+char *owner;
+};
 
-        return (0);
-}
+
+/**
+*dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
+#endif
